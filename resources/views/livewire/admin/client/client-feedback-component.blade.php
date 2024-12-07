8d06ccb8ed4a3615656d652ce362715e77b8878a
emon2021 <mdemonahmed2021@gmail.com>
2024-12-07 16:21:34 +0600

    add delete confirm with stopImediatePropagation

--- resources/views/livewire/admin/client/client-feedback-component.blade.php
+++ resources/views/livewire/admin/client/client-feedback-component.blade.php
@@ -65,7 +65,7 @@
                                                                 <i class="icon-edit"></i>
                                                             </div>
                                                         </a>
-                                                        <a href="javascript:void(0)" wire:click.prevent="destroyFeedback({{$feedback->id}})">
+                                                        <a href="javascript:void(0)" onclick="confirm('Are you sure to delete this feedback?') || event.stopImmediatePropagation()" wire:click.prevent="destroyFeedback({{$feedback->id}})">
                                                             <div class="item trash">
                                                                 <i class="icon-trash"></i>
                                                             </div>
